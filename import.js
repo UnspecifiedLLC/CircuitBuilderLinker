77dcdca36fc5de2072026c75713f5304e3e1c600
Shannon Code <genecyber@gmail.com>
2020-02-13 18:20:06 -0500

    monkey patch

--- import.js
+++ import.js
@@ -1,1 +1,29 @@
 console.log("Injected");
+function get(url) {
+    console.log("inside get");
+    url = atob(url);
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+    var template;
+    fetch(url, requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        template = result;
+        ImportDesigner(result);
+    })
+    .catch(error => {
+        console.log("Trying to import again, got an error", error);
+        setTimeout(()=>{get(btoa(url))},2000);
+    });
+}
+$(".apply-button").hide();
+var oldSetState = setState;
+setState = (value, noChange)=>{
+	!noChange && value && changedTab();
+    $('#designerstate').html(value);
+    $('.body').find('.apply-button').tclass('blink', value ? true : false);
+	value && setTimeout(()=>{EMIT('flow.apply')}, 500);
+}
+setTimeout(()=>{$("button.exec.onlyicon.red").hide()}, 2000);
