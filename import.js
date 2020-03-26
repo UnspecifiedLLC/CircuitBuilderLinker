d7fdd9c2e4e984cb2f6d8f8f478bd802fbe8451f
Shannon Code <genecyber@gmail.com>
2020-03-26 15:57:07 -0400

    Update import.js

--- import.js
+++ import.js
@@ -1,29 +1,40 @@
 console.log("Injected");
-function get(url) {
-    console.log("inside get");
-    url = atob(url);
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-    var template;
-    fetch(url, requestOptions)
-    .then(response => response.text())
-    .then(result => {
-        template = result;
-        ImportDesigner(result);
-    })
-    .catch(error => {
-        console.log("Trying to import again, got an error", error);
-        setTimeout(()=>{get(btoa(url))},2000);
-    });
+addCss("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css");
+function getIcons() {
+	// var classes = document.styleSheets[0].rules || document.styleSheets[0].cssRules;
+	var classes = Array();
+	Array.prototype.slice.call(document.styleSheets).forEach(function(sheet){
+		classes = classes.concat(Array.prototype.slice.call(sheet.cssRules));
+	});
+	var icons = {};
+	for (var x = 0; x < classes.length; x++) {
+		var cls = classes[x];
+		var sel = cls.selectorText;
+
+		if (sel && sel.substring(0, 4) == '.fa-') {
+			sel = sel.split(',');
+			var val = cls.cssText ? cls.cssText : cls.cssText.style.cssText;
+			var a = val.match(/[^\x00-\x7F]+/);
+			if (a)
+				sel.forEach(function(s){
+					s = s.trim();
+					s = s.substring(4, s.indexOf(':')).trim();
+					icons[s] = a.toString();
+				});
+		}
+	}
+	// console.log(icons);
+	return icons;
 }
-$(".apply-button").hide();
-var oldSetState = setState;
-setState = (value, noChange)=>{
-	!noChange && value && changedTab();
-    $('#designerstate').html(value);
-    $('.body').find('.apply-button').tclass('blink', value ? true : false);
-	value && setTimeout(()=>{EMIT('flow.apply')}, 500);
+
+function addCss(fileName) {
+
+  var head = document.head;
+  var link = document.createElement("link");
+
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.href = fileName;
+
+  head.appendChild(link);
 }
-setTimeout(()=>{$("button.exec.onlyicon.red").hide()}, 2000);
